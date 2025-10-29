72e38dcc86f67af3059cbafd53d589f23df877b2
Pavel Kholkin <xpaul6@gmail.com>
2025-10-30 02:02:52 +0500

    feat: theme switch icon

--- src/components/ThemeSwitch.svelte
+++ src/components/ThemeSwitch.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  import sun_img from "../assets/sun.png";
+  import moon_img from "../assets/moon.png";
+
   let { theme = $bindable<string>(), ...props } = $props();
 
   function changeTheme() {
@@ -22,5 +25,7 @@
 </script>
 
 <div class="fixed top-0 right-0 mt-3 mr-3 rounded-md hover:cursor-pointer">
-  <button class="p-1 hover:cursor-pointer" onclick={changeTheme}> S </button>
+  <button class="p-1 hover:cursor-pointer" onclick={changeTheme}>
+    <img src={theme == "dark" ? sun_img : moon_img} class="size-5" alt="S" />
+  </button>
 </div>
